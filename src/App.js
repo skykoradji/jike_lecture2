29419d98bb02110cc5f357de0e19802e6608191e
alex.you <alex.you@abc.net.au>
2019-07-27 14:03:47 +1000

    lecture2

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
 import React, { Component } from 'react';
 import math from 'mathjs';
 import './App.css';
-
-
-function Display({ data }) {
-  return <div className="Display">{data && data.join('')}</div>
-}
-
-function Buttons(props) {
-  return <div className="Buttons">{props.children}</div>
-}
-function Button({ onClick, size, value, label}) {
-  return (
-    <div onClick={onClick} className="Button" data-size={size} data-value={value}>
-      {label}
-    </div>
-  )
-}
-
-
+import Button from './Button';
+import Buttons from './Buttons';
+import Display from './Display';
 class App extends Component {
+
+  state = {
+    operations: []
+  };
+
+  calculation() {
+
+  }
+
+  handleClick = (event) => {
+    const value = event.target.getAttribute('data-value');
+    switch(value) {
+      case 'clear': 
+        this.setState({
+          operations: []
+        });
+        break;
+      case 'equal': 
+        this.calculation();
+        break;
+      default: 
+        this.setState((prevState) => {
+          prevState.operations.push(value);
+          return prevState;
+        });
+        break;
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <Display />
+        <Display data={this.state.operations}/>
         <Buttons>
-          <Button label="C" value="clear" />
-          <Button label="7" value="7" />
-          <Button label="4" value="4" />
-          <Button label="1" value="1" />
-          <Button label="0" value="0" />
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
 
-          <Button label="/" value="/" />
-          <Button label="8" value="8" />
-          <Button label="5" value="5" />
-          <Button label="2" value="2" />
-          <Button label="." value="." />
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
 
-          <Button label="x" value="*" />
-          <Button label="9" value="9" />
-          <Button label="6" value="6" />
-          <Button label="3" value="3" />
-          <Button label="" value="null" />
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
+          <Button onClick={this.handleClick} label="" value="null" />
 
-          <Button label="-" value="-" />
-          <Button label="+" size="2" value="+" />
-          <Button label="=" size="2" value="equal" />
+          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="+" size="2" value="+" />
+          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
         </Buttons>
       </div>
     )
